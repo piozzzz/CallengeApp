93f78e3c1bd44d18551cad8a845f5722d9b0dc19
Piotr <ppzak@go2.pl>
2023-10-02 19:51:57 +0200

    zadanie dzien 4

--- CallengApp/CallengApp/Program.cs
+++ CallengApp/CallengApp/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//==============================================================================
+// Dzien 4 - zadanie
+
+//dane do podania:
+string yourName = "Ewa";  // podaj imie:        dowolne imie
+char yourSex = 'K';       // podaj płac:        K - kobieta, M- Meszczyżna , .... dowolna płec , ==> podaj płac odpowiednia dla imienia
+var yourAge = 33;         // podaj ile ma lat:  od 0 do nieskonczonosci
+
+
+// kod - start
+
+if (yourSex == 'K')  //wybór kobiety
+{
+    if (yourAge >= 30 && yourName == "Ewa")
+    { Console.WriteLine(yourName + ", lat " + yourAge); }
+
+    else if (yourAge >= 30)
+    { Console.WriteLine("Kobieta, lat " + yourAge); }
+
+    else if (yourAge < 30 && yourAge >= 18)
+    { Console.WriteLine("Kobieta poniżej 30 lat"); }
+
+    else if (yourAge < 18)
+    { Console.WriteLine("Niepełnoletnia Kobieta"); }
+}
+
+if (yourSex == 'M') // wybór męszczyzny
+{
+    if (yourAge >= 30 && yourName == "Piotr")
+    { Console.WriteLine(yourName + ", lat " + yourAge); }
+
+    else if (yourAge >= 30)
+    { Console.WriteLine("Meszczyża, lat " + yourAge); }
+
+    else if (yourAge < 30 && yourAge >= 18)
+    { Console.WriteLine("Męszczyzna poniżej 30 lat"); }
+
+    else if (yourAge < 18)
+    { Console.WriteLine("Niepełnoletni Męszczyzna"); }
+}
+
+if (yourSex != 'K' && yourSex != 'M') // wybór LGTB
+{
+    if (yourAge >= 30)
+    { Console.WriteLine("LGTB, lat " + yourAge); }
+
+    else if (yourAge < 30 && yourAge >= 18)
+    { Console.WriteLine("LGTB poniżej 30 lat"); }
+
+    else if (yourAge < 18)
+    { Console.WriteLine("Niepełnoletni LGTB"); }
+}
+
+// kod - koniec
+//=========================================================================
